2a3f3456f69d9ab9bd3ab7e0fe0d705208cebf4a
J <Mohammadiqbalramadhan21@gmail.com>
2024-01-12 17:09:51 +0700

    fog: more xdroid bringup

--- device.mk
+++ device.mk
@@ -356,7 +356,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-xdroid
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
